6e69bc832c4722c2011be14c8c69e4feab0e5452
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-07-28 10:06:22 +0200

    refactor: neural networks

--- agents/networks.py
+++ agents/networks.py
@@ -1,41 +1,50 @@
 from keras import layers
-from tensorflow import keras
-import flappy_bird_gym
-import utils
+import tensorflow as tf
 
 
-def get_a2c_network(obs_shape, num_actions):
-    """
-    Generate the actor-critic network
-    :param obs_shape: shape of gym observation space
-    :param num_actions: number of actions in gym action space of the environment
-    :return actor network and critic network
-    """
+# Neural network for FlappyBird-v0 environment
+class ActorCriticBase(tf.keras.Model):
 
-    input = layers.Input(shape=obs_shape)
-    x = layers.Conv2D(16, (3, 3), kernel_regularizer='l2', activation='relu')(input)
-    x = layers.MaxPool2D((2, 2))(x)
-    x = layers.Conv2D(32, (3, 3), kernel_regularizer='l2', activation='relu')(x)
-    x = layers.MaxPool2D((2, 2))(x)
-    x = layers.Conv2D(64, (3, 3), kernel_regularizer='l2', activation='relu')(x)
-    x = layers.MaxPool2D((2, 2))(x)
-    x = layers.Flatten()(x)
-    x = layers.Dense(64, kernel_regularizer='l2', activation='relu')(x)
-    x = layers.Dense(32, kernel_regularizer='l2', activation='relu')(x)
-    actions = layers.Dense(num_actions, activation='softmax')(x)
-    value = layers.Dense(1, activation='relu')(x)
-    # actor-critic network
-    actor = keras.Model(input, actions, name="actor")
-    critic = keras.Model(input, value, name="critic")
-    return actor, critic
+    def __init__(
+            self,
+            num_actions: int):
+        super().__init__()
+
+        self.gru1 = layers.GRU(5)
+        self.dense1 = layers.Dense(4, activation='relu')
+        self.dense2 = layers.Dense(4, activation='relu')
+        self.dense3 = layers.Dense(2, activation='relu')
+        self.actor = layers.Dense(num_actions, activation='softmax')
+        self.critic = layers.Dense(1)
+
+    def call(self, inputs: tf.Tensor):
+        x = self.gru1(inputs)
+        x = self.dense1(x)
+        x = self.dense2(x)
+        x = self.dense3(x)
+        return self.actor(x), self.critic(x)
 
 
-if __name__ == "__main__":
+# Neural network for FlappyBird-rb-v0 environment
+class ActorCriticCNN(tf.keras.Model):
 
-    env = flappy_bird_gym.make(utils.FLAPPY_BIRD_ENV)
-    obs_shape, num_actions = utils.extract_spaces(env, decompose=True)
+    def __init__(
+            self,
+            num_actions: int):
+        super().__init__()
 
-    actor, critic = get_a2c_network(obs_shape, num_actions)
+        self.conv1 = layers.Conv2D(32, (8, 8), strides=4, activation='relu')
+        self.conv2 = layers.Conv2D(64, (4, 4), strides=2, activation='relu')
+        self.conv3 = layers.Conv2D(64, (3, 3), activation='relu')
+        self.flatten1 = layers.Flatten()
+        self.dense1 = layers.Dense(512, activation='relu')
+        self.actor = layers.Dense(num_actions, activation='softmax')
+        self.critic = layers.Dense(1)
 
-    actor.summary()
-    critic.summary()
+    def call(self, inputs: tf.Tensor):
+        x = self.conv1(inputs)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.flatten1(x)
+        x = self.dense1(x)
+        return self.actor(x), self.critic(x)
